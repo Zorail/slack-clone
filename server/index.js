9bdfa97ecb9ea06c9fb7e1bd9251e0767204f47c
Rohan <rohan.seth497@gmail.com>
2018-07-14 22:54:01 +0530

    splitted schema and resolvers

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
-
-import typeDefs from './schema';
-import resolvers from './resolvers';
+import path from 'path';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 
 import models from './models';
+
+
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
+
+const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
 
 const schema = makeExecutableSchema({
   typeDefs,
@@ -17,11 +21,12 @@ const app = express();
 
 const graphqlEndpoint = '/graphql';
 
-app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress({ schema }));
+// Context to get models of sequelize to our resolvers
+app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress({ schema, context: { models } }));
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
 
-models.sequelize.sync({ force: true }).then(() => {
+models.sequelize.sync().then(() => {
   app.listen(8080);
 });
 
