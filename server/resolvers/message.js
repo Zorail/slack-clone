168f81cb729ce38eb7a4f4792673cd51e37c37f2
Rohan <rohan.seth497@gmail.com>
2018-07-15 11:02:38 +0530

    added react frontend

--- server/resolvers/message.js
+++ server/resolvers/message.js
@@ -1,3 +1,14 @@
 export default {
-
+  Mutation: {
+    createMessage: async (parent, args, { models, user }) => {
+      try {
+        await models.Message.create({
+          ...args,
+          userId: user.id,
+        });
+      } catch (e) {
+        return false;
+      }
+    },
+  },
 };
