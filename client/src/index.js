80e1fd1e3239c7039d44c41c00ce6bb1aad2df18
Rohan <rohan.seth497@gmail.com>
2018-07-22 10:15:56 +0530

    testing

--- client/src/index.js
+++ client/src/index.js
@@ -1,47 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import ApolloClient from 'apollo-boost';
-import { ApolloClient } from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
-import { createHttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
-import Routes from './routes';
-// import registerServiceWorker from './registerServiceWorker';
-
-// import stylesheet
+import { ApolloClient, createNetworkInterface, ApolloProvider } from 'react-apollo';
 import 'semantic-ui-css/semantic.min.css';
 
-const httpLink = createHttpLink({
+import Routes from './routes/index';
+import registerServiceWorker from './registerServiceWorker';
+
+const networkInterface = createNetworkInterface({
   uri: 'http://localhost:8081/graphql',
-  credentials: 'include'
 });
 
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    }
-  }
-});
+networkInterface.use([
+  {
+    applyMiddleware(req, next) {
+      if (!req.options.headers) {
+        req.options.headers = {};
+      }
+
+      req.options.headers['x-token'] = localStorage.getItem('token');
+      req.options.headers['x-refresh-token'] = localStorage.getItem('refreshToken');
+      next();
+    },
+  },
+]);
+
+networkInterface.useAfter([
+  {
+    applyAfterware({ response: { headers } }, next) {
+      const token = headers.get('x-token');
+      const refreshToken = headers.get('x-refresh-token');
+
+      if (token) {
+        localStorage.setItem('token', token);
+      }
+
+      if (refreshToken) {
+        localStorage.setItem('refreshToken', refreshToken);
+      }
+
+      next();
+    },
+  },
+]);
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
+  networkInterface,
 });
 
-console.log("Client", client);
-
-const App = () => (
+const App = (
   <ApolloProvider client={client}>
     <Routes />
   </ApolloProvider>
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+ReactDOM.render(App, document.getElementById('root'));
+registerServiceWorker();
